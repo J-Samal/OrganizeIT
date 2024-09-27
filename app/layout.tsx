bca6b5837c62115fc94e18f410c35fb78e954ccb
Janmejoy <samaljanmejoy2002@gmail.com>
2024-09-28 01:41:43 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: 'OrganizeIT',
+  description:'OrganizeIt is a organizer platform.',
+  icons: {
+    icon: '/assets/images/logo.svg'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
