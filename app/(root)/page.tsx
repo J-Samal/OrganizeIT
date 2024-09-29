8ffc949f70468e2b90ed220b839bd6e3f416ba45
Janmejoy <samaljanmejoy2002@gmail.com>
2024-09-29 22:45:48 +0530

    implement stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
-// import CategoryFilter from '@/components/shared/CategoryFilter';
-// import Collection from '@/components/shared/Collection'
-// import Search from '@/components/shared/Search';
-// import { Button } from '@/components/ui/button'
-// import { getAllEvents } from '@/lib/actions/event.actions';
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
@@ -12,12 +12,12 @@ export default async function Home({ searchParams }: SearchParamProps) {
   const searchText = (searchParams?.query as string) || '';
   const category = (searchParams?.category as string) || '';
 
-  // const events = await getAllEvents({
-  //   query: searchText,
-  //   category,
-  //   page,
-  //   limit: 6
-  // })
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
 
   return (
     <>
@@ -26,11 +26,11 @@ export default async function Home({ searchParams }: SearchParamProps) {
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
             <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
-            {/* <Button size="lg" asChild className="button w-full sm:w-fit">
+            <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
                 Explore Now
               </Link>
-            </Button> */}
+            </Button>
           </div>
 
           <Image 
@@ -43,7 +43,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         </div>
       </section> 
 
-      {/* <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
@@ -60,7 +60,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           page={page}
           totalPages={events?.totalPages}
         />
-      </section> */}
+      </section>
     </>
   )
 }
