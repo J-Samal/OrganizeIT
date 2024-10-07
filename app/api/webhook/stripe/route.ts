4d729e1770bc2dcedd3b2442742953a04b2b2f38
Janmejoy <samaljanmejoy2002@gmail.com>
2024-10-07 15:19:21 +0530

    final commit

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -6,7 +6,7 @@ export async function POST(request: Request) {
   const body = await request.text()
 
   const sig = request.headers.get('stripe-signature') as string
-  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
+  const endpointSecret = "whsec_jrYoFgl4TgeMC1DuU45jafzgVel8vn6m"
 
   let event
 
